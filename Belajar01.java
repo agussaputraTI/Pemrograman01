aaef38b0f4d20b0d56a665a14bfb47f264ee5e3b
Agus Saputra <31505686+agussaputraTI@users.noreply.github.com>
2017-09-04 19:54:38 -0800

    Add files via upload

---
+++ Belajar01.java
@@ -0,0 +1,8 @@
+public class Belajar01{
+
+     public static void main(String []args){
+        System.out.println("\nNama : Agus Saputra");
+        System.out.println("Nim : 17220037 \n");
+        
+     }
+}
